8164921c3d40c94b64ea7244ab496269cdc50423
Joao Santos <ctw01895@criticaltechworks.com>
2022-04-04 15:31:55 +0100

    Ex01 done.

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+#include <cmath>
 #include "Fixed.h"
 
 //==============================================================================
@@ -14,18 +16,51 @@ Fixed::Fixed(const Fixed& src) {
 	*this = src;
 }
 
+/*
+ * Comment taken and adapted from the repo of dcavalei:
+ *	The int raw_bits is being used to store a value, with the first 24 bits
+ *	reserved to the sign and integer part and the last 8 bit to the fractional
+ *	part. Therefore, the maximum number we can store is 8bits shorter than the
+ *	MAX_INT. Note that, in the following diagram, the bits with a 0 on MUST be 0
+ *	otherwise it will be out of range. Not sure if the first bit must also be 0,
+ *	since it stores the sign info.
+ *							  |
+ *							 \|/
+ *	bits before:	x0000000 0xxxxxxx xxxxxxxx xxxxxxxx
+ *	bits after:		xxxxxxxx xxxxxxxx xxxxxxxx 00000000
+ *					/|\						  |........| > fractal part
+ *					 |
+ */
+
 Fixed::Fixed(const int value) {
-//	It converts it to the corresponding fixed-point value. The fractional_bits bits value is
-//	initialized to 8 like in exercise 00.
+	std::cout << "Int constructor called.\n";
+	raw_bits = value << fractional_bits;
+	if (value > (INT_MAX >> fractional_bits))
+		std::cout << "WARNING: The value "
+				  << value
+				  << " is out of range, undefined behaviour from now on.\n";
 }
 
+/*
+ *	The float bitwise representation in different from the one for ints. Using
+ *	a fixed-point number approach though, one can do the bitwise shift by
+ *	multiplying or dividing by 2^x, where x is the number of places shift (see
+ *	https://inst.eecs.berkeley.edu/~cs61c/sp06/handout/fixedpt.html). So, here
+ *	it creates an int with the value 2^8 (the '1 << _fractal_bit_amount') which
+ *	will be used to do the bit shifting.
+*/
+
 Fixed::Fixed(const float value) {
-//It converts it to the corresponding fixed-point value. The fractional_bits bits value is
-//initialized to 8 like in exercise 00.
+	std::cout << "Float constructor called.\n";
+	raw_bits = roundf(value * (1 << fractional_bits));
+	if (value > (INT_MAX >> fractional_bits))
+		std::cout << "WARNING: The value "
+				  << value
+				  << " is out of range, undefined behaviour from now on.\n";
 }
 
 //==============================================================================
-// 	Destructor
+// 	Destructors
 //==============================================================================
 
 Fixed::~Fixed() {
@@ -43,31 +78,37 @@ Fixed &Fixed::operator=(const Fixed& rh_instance) {
 	return (*this);
 }
 
-std::ostream& operator<<(std::ostream &ostream, const Fixed& obj)
-{}
+std::ostream& operator<<(std::ostream &os, const Fixed& obj) {
+	os << obj.toFloat();
+	return os;
+}
 
 //==============================================================================
 //	Methods of the Fixed class.
 //==============================================================================
 
-int	Fixed::getRawBits() const
-{
+int	Fixed::getRawBits() const {
 	std::cout << "getRawBits member function called.\n";
 	return (this->raw_bits);
 }
 
-void	Fixed::setRawBits(const int raw)
-{
+void	Fixed::setRawBits(const int raw) {
 	std::cout << "getRawBits member function called.\n";
 	this->raw_bits = raw;
 }
 
-float Fixed::toFloat() const
-{
-	// converts the fixed-point value to a floating-point value.
+/*
+ * Converts the raw_bits value to a float and returns it. Note that you cannot
+ * just right shift and then cast to float, otherwise the compiler will just
+ * trim the fractional part before the casting.
+ */
+
+float Fixed::toFloat() const {
+	return ((float)this->raw_bits / (1 << fractional_bits));
 }
 
-int Fixed::toInt() const
-{
-	// converts the fixed-point value to an integer value.
+/* Returns the integer part of the raw_bits value. */
+
+int Fixed::toInt() const {
+	return (this->raw_bits >> fractional_bits);
 }
--- module02/ex01/Fixed.h
+++ module02/ex01/Fixed.h
@@ -1,17 +1,16 @@
 #ifndef EX00_FIXED_H
 #define EX00_FIXED_H
 
-
 class Fixed {
 public:
-	// Constructors
+	/* ============= CONSTRUCTORS ============= */
 	Fixed();
 	Fixed(const Fixed& src);
-	Fixed(const int value);
-	Fixed(const float value);
-	// Destructors
+	explicit Fixed(const int value);
+	explicit Fixed(const float value);
+	/* ============== DESTRUCTORS ============= */
 	virtual ~Fixed();
-	// Operator overloads
+	/* ========== OPERATOR OVERLOADS ========== */
 	Fixed& operator=(const Fixed& rh_instance);
 
 	int					getRawBits() const;
@@ -25,11 +24,11 @@ private:
 
 /*
  * As stated in https://en.cppreference.com/w/cpp/language/operators, the
- * overloads of operator '>>' and '<<' that take std::istream& or std::ostream&
+ * overloads of operator '>>' and '<<' that take std::istream& or std::os&
  * as the left hand argument (AKA insertion and extraction operators) must be
  * implemented as non-members, since they take the user-defined type as the
  * right hand argument.
  */
-std::ostream& operator<<(std::ostream& ostream, const Fixed& obj);
+std::ostream& operator<<(std::ostream& os, const Fixed& obj);
 
 #endif //EX00_FIXED_H
