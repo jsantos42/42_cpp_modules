570ddb25bee3b33b042c4b3ecbcbecbb74a05455
Joao Santos <ctw01895@criticaltechworks.com>
2022-04-06 19:10:22 +0100

    Cleaned code.

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -4,7 +4,11 @@
 // 	CONSTRUCTORS
 //==============================================================================
 ClapTrap::ClapTrap() {
-	std::cout << "Default constructor called.\n";
+	std::cout << "[CLAPTRAP] Default constructor called.\n";
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src) {
+	*this = src;
 }
 
 ClapTrap::ClapTrap(const std::string& name)
@@ -12,11 +16,8 @@ ClapTrap::ClapTrap(const std::string& name)
 	hit_points(10),
 	energy_points(10),
 	attack_damage(0) {
-	std::cout << "String constructor called.\n";
+	std::cout << "[CLAPTRAP] String constructor called.\n";
 }
-
-ClapTrap::ClapTrap(const ClapTrap &src)
-{ *this = src; }
 
 
 //==============================================================================
@@ -24,8 +25,9 @@ ClapTrap::ClapTrap(const ClapTrap &src)
 //==============================================================================
 
 ClapTrap::~ClapTrap() {
-	std::cout << "Destructor called.\n";
+	std::cout << "[CLAPTRAP] Destructor called.\n";
 }
+
 
 //==============================================================================
 // 	OPERATOR OVERLOADS
@@ -41,23 +43,10 @@ ClapTrap&	ClapTrap::operator=(const ClapTrap &rh_instance) {
 	return (*this);
 }
 
+
 //==============================================================================
 // 	METHODS OF THE ClapTrap CLASS.
 //==============================================================================
-
-/*
- *When ClapTrack attacks, it causes its target to lose <attack damage> hit points.
-When ClapTrap repairs itself, it gets <amount> hit points back. Attacking and repairing
-cost 1 energy point each. Of course, ClapTrap can’t do anything if it has no hit points
-or energy points left.
-In all of these member functions, you have to print a message to describe what hap-
-pens. For example, the attack() function may display something like (of course, without
-the angle brackets):
-ClapTrap <name> attacks <target>, causing <damage> points of damage!
-The constructors and destructor must also display a message, so your peer-evaluators
-can easily see they have been called.
-Implement and turn in your own tests to ensure your code works as expected.
- */
 
 void	ClapTrap::attack(const std::string &target) {
 	std::cout << "ClapTrap named "
