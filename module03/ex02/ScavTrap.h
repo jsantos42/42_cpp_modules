5e587290ff2ce16f87de57045537569916062bc5
Joao Santos <ctw01895@criticaltechworks.com>
2022-04-08 18:51:40 +0100

    Ex02 done.

---
+++ module03/ex02/ScavTrap.h
@@ -0,0 +1,17 @@
+#ifndef EX01_SCAVTRAP_H
+#define EX01_SCAVTRAP_H
+
+#include "ClapTrap.h"
+
+class ScavTrap : public ClapTrap {
+public:
+	ScavTrap();                       			// Default constructor
+	ScavTrap(const ScavTrap &src);    			// Copy constructor
+	explicit ScavTrap(const std::string& name);
+	~ScavTrap();                				// Default destructor
+
+	ScavTrap&	operator=(const ScavTrap& rh_instance);    // Assign operator overload
+	void		guardGate();
+};
+
+#endif //EX01_SCAVTRAP_H
