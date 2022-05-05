5d2879fc0cfcb24fc8e59d6ac033720d9d6895b2
jsantos <jsantos@student.42lisboa.com>
2022-05-05 18:24:40 +0100

    Missing float and double printing.

--- module06/ex00/Conversions.cpp
+++ module06/ex00/Conversions.cpp
@@ -12,6 +12,7 @@ Conversions::Conversions() : type(NAN) {
 Conversions::Conversions(const std::string& str) {
 	std::cout << "[Conversions] String constructor.\n";
 	storeType(str);
+	storeStringNumber(str);
 	printChar();
 	printInt();
 	printFloat();
@@ -48,7 +49,9 @@ Conversions &Conversions::operator=(const Conversions &rh_instance) {
 //==============================================================================
 
 void	Conversions::storeType(const std::string& str) {
-	if (isChar(str))
+	if (isSpecial(str))
+		type = SPECIAL;
+	else if (isChar(str))
 		type = CHAR;
 	else if (isInt(str))
 		type = INT;
@@ -58,12 +61,48 @@ void	Conversions::storeType(const std::string& str) {
 		type = DOUBLE;
 }
 
+void Conversions::storeStringNumber(const std::string &str) {
+	std::istringstream	stream(str);
+	char				c;
+
+	string = str;
+	if (getType() == SPECIAL) {
+		if (str == "-inff" || str == "+inff" || str == "nanf")
+			string = str.substr(0, str.length() - 1);
+	}
+	else if (getType() == INT || getType() == FLOAT || getType() == DOUBLE)
+		stream >> number;
+	else if (getType() == CHAR) {
+		stream >> c;
+		number = c;
+	}
+	else
+		number = 0;
+}
+
 int Conversions::getType() const {
 	return (type);
 }
 
+double Conversions::getNumber() const {
+	return (number);
+}
+
 std::string Conversions::getString() const {
 	return (string);
+}
+
+//==============================================================================
+//	IS FUNCTIONS
+//==============================================================================
+
+bool Conversions::isSpecial(const std::string &str) {
+	std::string special_cases[] = {"-inf", "+inf", "nan"};
+
+	for (unsigned long i = 0; i < special_cases->length(); i++)
+		if (str == special_cases[i] || str == (special_cases[i] + "f"))
+			return (true);
+	return (false);
 }
 
 bool Conversions::isChar(const std::string &str) {
@@ -71,7 +110,7 @@ bool Conversions::isChar(const std::string &str) {
 }
 
 /*
- *	Using an istringstream (input stream that operates on strings)
+ *	Stream output from an istringstream (input stream that operates on strings)
  *	eof() makes sure the string has ended.
  *	fail() informs if an error occurred during the output operation
  */
@@ -82,13 +121,65 @@ bool Conversions::isInt(const std::string &str) {
 	stream >> i;
 	return (stream.eof() && !stream.fail());
 
-	while (std::cin.fail()){
-		std::cin.clear();
-		std::cin.ignore();
-		if (str == "SEARCH")
-			return (-1);
-		std::cout << std::endl << "Wrong input, please provide a valid number: ";
-		std::cin >> input;
-	}
+//	while (std::cin.fail()){
+//		std::cin.clear();
+//		std::cin.ignore();
+//		if (str == "SEARCH")
+//			return (-1);
+//		std::cout << std::endl << "Wrong input, please provide a valid number: ";
+//		std::cin >> input;
+//	}
+}
+
+bool Conversions::isFloat(const std::string &str) {
+	std::string			numeric = str.substr(0, str.length() - 1);
+	std::string			alpha = str.substr(str.length() - 1, 1);
+	std::istringstream	stream(numeric);
+	int					point = 0;
+	float				f;
+
+	for (unsigned long i = 0; i < numeric.length(); i++)
+		if (numeric[i] == '.')
+			point++;
+
+	stream >> f;
+	return (stream.eof() && !stream.fail() && point == 1 && alpha == "f");
+}
+
+bool Conversions::isDouble(const std::string &str) {
+	std::istringstream	stream(str);
+	double				d;
+
+	stream >> d;
+	return (stream.eof() && !stream.fail());
+}
+
+//==============================================================================
+//	PRINT FUNCTIONS
+//==============================================================================
+
+void Conversions::printChar() const {
+	std::cout << "char: ";
+	if (getType() == NAN || getType() == SPECIAL)
+		std::cout << "impossible\n";
+	else if (std::isprint(static_cast<int>(getNumber())))
+		std::cout << '\'' << static_cast<char>(getNumber()) << '\'' << std::endl;
+	else
+		std::cout << "non printable char\n";
+}
+
+void Conversions::printInt() const {
+	std::cout << "int: ";
+	if (getType() == NAN || getType() == SPECIAL)
+		std::cout << "impossible\n";
+	else
+		std::cout << static_cast<int>(getNumber()) << std::endl;
+}
+
+void Conversions::printFloat() const {
+
+}
+
+void Conversions::printDouble() const {
 
 }
--- module06/ex00/Conversions.h
+++ module06/ex00/Conversions.h
@@ -6,6 +6,7 @@
 
 enum e_type {
 	NAN,
+	SPECIAL,
 	CHAR,
 	INT,
 	FLOAT,
@@ -20,12 +21,21 @@ public:
 
 	Conversions&	operator=(const Conversions &rh_instance);
 	void			storeType(const std::string& str);
+	void			storeStringNumber(const std::string& str);
 	int				getType() const;
+	double			getNumber() const;
 	std::string 	getString() const;
+	//==========================================================================
+	// IS FUNCTIONS
+	//==========================================================================
+	bool			isSpecial(const std::string& str);
 	bool			isChar(const std::string& str);
 	bool			isInt(const std::string& str);
 	bool			isFloat(const std::string& str);
 	bool			isDouble(const std::string& str);
+	//==========================================================================
+	// PRINT FUNCTIONS
+	//==========================================================================
 	void			printChar() const;
 	void			printInt() const;
 	void			printFloat() const;
@@ -34,6 +44,7 @@ public:
 private:
 	Conversions();
 	int			type;
+	double		number;
 	std::string	string;
 };
 
