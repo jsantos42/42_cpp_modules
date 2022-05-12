20f969c22322054227468a86776abbde73fb9720
Joao Santos <ctw01895@criticaltechworks.com>
2022-05-12 17:55:38 +0100

    Ex00 done.

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -2,20 +2,48 @@
 #include "whatever.h"
 
 int	main() {
+	std::cout << "============================================================\n";
+	std::cout << "FROM THE SUBJECT (adapted)\n";
+	std::cout << "============================================================\n";
 	int			a = 2;
 	int 		b = 3;
-	std::string	c = "chaine1";
-	std::string d = "chaine2";
-
-	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << std::endl;
 
-	::swap(c, d);
+	std::string	c = "chaine1";
+	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << std::endl;
+
+	float	e = 2.01f;
+	float	f = 3.01f;
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	swap( e, f );
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "============================================================\n";
+	std::cout << "FROM THE CORRECTION SHEET\n";
+	std::cout << "============================================================\n";
+
+	Awesome	g(2), h(4);
+	std::cout << g << " " << h << std::endl;
+	swap(g, h);
+	swap<Awesome>(g, h);
+	swap<Awesome>(g, h);
+	std::cout << g << " " << h << std::endl;
+	std::cout << max(g, h) << std::endl;
+	std::cout << min(g, h) << std::endl;
 
 	return 0;
 }
--- module07/ex00/whatever.h
+++ module07/ex00/whatever.h
@@ -1,11 +1,34 @@
 #ifndef EX00_WHATEVER_H
 #define EX00_WHATEVER_H
 
+class Awesome {
+	public:
+		Awesome( int n ) : _n(n) {}
+
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const { return (this->_n != rhs._n); }
+		bool operator> ( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator< ( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+
+		int get_n() const { return _n; }
+	private:
+		int _n;
+};
+
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a) {
+	o << a.get_n();
+	return o; 
+}
+
+/* Notice that here tmp must be initialized when declared, otherwise it might
+ * throw an error if A is a class with a private default constructor. */
 template<typename A>
 void	swap(A& a1, A& a2) {
-	A	tmp;
+	A	tmp = a1;
 
-	tmp = a1;
 	a1 = a2;
 	a2 = tmp;
 }
