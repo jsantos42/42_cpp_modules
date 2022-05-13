3f4a93a1d2fcb83730ceea096213f4e7b58e22c2
Joao Santos <ctw01895@criticaltechworks.com>
2022-05-13 13:18:02 +0100

    Ex01 done.

--- module07/ex01/iter.h
+++ module07/ex01/iter.h
@@ -1,9 +1,7 @@
 #ifndef EX01_ITER_H
 #define EX01_ITER_H
 
-//#include <cstdlib>
 #include <iostream>
-
 
 template<typename T, typename F>
 void	iter(T* array, size_t len, F fun) {
@@ -15,6 +13,5 @@ template<typename T>
 void	output(const T& element) {
 	std::cout << element << std::endl;
 }
-
 
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,10 +1,46 @@
 #include "iter.h"
 
-int	main() {
-	std::string	str1 = "ola";
-//	char*	str = "ola";
+//==============================================================================
+// FROM THE CORRECTION SHEET
+//==============================================================================
+class Awesome {
+public:
+	Awesome( void ): _n(42) { return; }
+	int	get( void ) const { return this->_n; }
 
-	iter(str1, 3, output);
+private:
+	int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template <typename T>
+void	print( const T& x ) { std::cout << x << std::endl; return; }
+
+
+
+//==============================================================================
+// MAIN
+//==============================================================================
+
+int	main() {
+	char	c[] = "ola";
+	int		i[] = {1, 2, 3};
+	float	f[] = {1.1, 2.2, 3.3};
+
+	iter(c, 3, output<char>);
+	iter(i, 3, output<int>);
+	iter(f, 3, output<float>);
+
+	std::cout << "============================================================\n";
+	std::cout << "FROM THE CORRECTION SHEET\n";
+	std::cout << "============================================================\n";
+
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter( tab, 5, print<int> );
+	iter( tab2, 5, print<Awesome> );
 
 	return 0;
 }
