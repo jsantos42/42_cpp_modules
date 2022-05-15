e072f15bd2602358e8f7379dbafdeb932e642b42
Joao Santos <ctw01895@criticaltechworks.com>
2022-05-15 14:15:18 +0100

    Ex00 done.

--- module08/ex00/easyfind.h
+++ module08/ex00/easyfind.h
@@ -1,18 +1,37 @@
 #ifndef EX00_EASYFIND_H
 #define EX00_EASYFIND_H
 
+#include <algorithm>
+#include <sstream>
+
+/* Note that 'T::const_iterator' is a non-defined type, which is dependent on T;
+ hence, everytime we use it, we need to declare 'typename' prior to it. */
 template<typename T>
-unsigned int	easyfind(const T& container, int to_find) {
-	unsigned int	i;
-	unsigned int	ie;
+typename T::const_iterator	easyfind(const T& int_container, int to_be_found) {
+	typename T::const_iterator	i;
+	typename T::const_iterator	ie;
 
-	i = container.begin();
-	ie = container.end();
+	i = int_container.begin();
+	ie = int_container.end();
 
-	if (i != ie)
-	{return (to_find);}
-	return i;
-
+	return (std::find(i, ie, to_be_found));
 }
 
-#endif //INC_42_CPP_MODULES_EASYFIND_H
+/* An iterator can be dereferenced. */
+template<typename T>
+void	printSearchResults(const T& int_container, int to_search) {
+	typename T::const_iterator	i;
+	std::string					str;
+	std::stringstream 			stream;
+
+	i = easyfind(int_container, to_search);
+	if (i != int_container.end()) {
+		stream << std::string(" is on index ") << *i;
+		str = stream.str();
+	}
+	else
+		str = " is NA";
+	std::cout << "The number " << to_search << str << ".\n";
+}
+
+#endif //EX00_EASYFIND_H
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -1,12 +1,32 @@
 #include <list>
+#include <vector>
+#include <deque>
+#include <iostream>
 #include "easyfind.h"
 
-int	main() {
-	std::list<int> l(3);
+int i = 0;
 
-	l.push_back(1);
-	l.push_back(2);
-	l.push_back(3);
-	easyfind(l, 1);
+int	fillContainer() {
+	return (i++);
+}
+
+int	main() {
+	std::list<int>		l(3);
+	std::vector<int>	v(3);
+	std::deque<int>		d(3);
+
+	std::generate(l.begin(), l.end(), fillContainer);
+	std::generate(v.begin(), v.end(), fillContainer);
+	std::generate(d.begin(), d.end(), fillContainer);
+
+	std::cout << "======== List: ========\n";
+	for (int j = 0; j < 10; j++)
+		printSearchResults(l, j);
+	std::cout << "======== Vector: ========\n";
+	for (int j = 0; j < 10; j++)
+		printSearchResults(v, j);
+	std::cout << "======== Double ended queue: ========\n";
+	for (int j = 0; j < 10; j++)
+		printSearchResults(d, j);
 	return 0;
 }
